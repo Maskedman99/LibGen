87bb237bcbd92df63d152df6d30a41dbff639ab7
Rohit <rohitmpaul@gmail.com>
2019-06-28 23:50:48 +0530

    Comics Menu need work

--- Screens/Comics.js
+++ Screens/Comics.js
@@ -5,13 +5,22 @@ import {Text, TouchableRipple, Appbar, Provider as PaperProvider} from 'react-na
 export class Comics extends Component {
 
   state ={
-    searchQuery: 'hello'
+    searchQuery: 'hello',
+    root: '',
+    loading: true,
   }
 
   componentDidMount(){
     const { navigation } = this.props;
     const search = navigation.getParam('search', '');
     this.setState({searchQuery: search})
+    
+    axios.get("http://gen.lib.rus.ec/comics/index.php?s=" + search.replace(' ','+'))
+    .then(data => this.setState({
+                          root: HTMLParser.parse(data.data),
+                          loading: false
+                          }))
+    .catch(err => alert('Something went wrong! Check your connection.'));
   }
 
   render() {
